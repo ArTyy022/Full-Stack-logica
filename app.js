abf8b171fb79832558c6d2adc86c363e22f51f53
ArTyy022 <toledoarthur11@gmail.com>
2025-07-07 11:38:18 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,22 @@
+/*alert('Boas-vindas ao jogo do número secreto')
+ let chute = prompt('Escolha um número de 1 e 10')
+
+ let numeroSecreto = 4
+
+ if (chute == numeroSecreto) {
+    alert('Acertou')
+ }
+*/
+
+
+alert('Boas vindas ao nosos site')
+
+let nome = "Lua";
+let idade = 25;
+let numeroDeVendas = 50;
+let saldoDisponivel = 1000;
+alert ('Erro! Preencha todos os campos');
+let mensagemDeErro = 'Erro! Preencha todos os campos';
+alert(mensagemDeErro);
+
+
