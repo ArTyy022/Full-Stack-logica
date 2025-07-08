97b1b8d0bed988904bfe7a6c8b127e741d2d15b1
ArTyy022 <toledoarthur11@gmail.com>
2025-07-08 11:54:43 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -9,14 +9,24 @@
 */
 
 
-alert('Boas vindas ao nosos site')
+/*alert('Bem-vindo ao nosso site!');
 
 let nome = "Lua";
+
 let idade = 25;
+
 let numeroDeVendas = 50;
+
 let saldoDisponivel = 1000;
-alert ('Erro! Preencha todos os campos');
+
+alert('Erro! Preencha todos os campos');
+
 let mensagemDeErro = 'Erro! Preencha todos os campos';
-alert(mensagemDeErro);
+alert(mensagemDeErro);*/
+let nome = prompt('Digite o seu nome');
 
+let idade = prompt('Digite a sua idade');
 
+if (idade >= 18) {
+    alert('Pode tirar a habilitação!'); 
+}
