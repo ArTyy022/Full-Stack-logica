40290ed34124e2355720a4cab90f0f29aa4b8a99
ArTyy022 <toledoarthur11@gmail.com>
2025-07-09 11:18:17 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -4,10 +4,13 @@
  let numeroSecreto = 4
 
  if (chute == numeroSecreto) {
-    alert('Acertou')
+    console.log('Isso ai você descobriu o número secreto(4)');
+    alert('Você acertou o número secreto' + numeroSecreto)
  }
+else {
+    alert('Você errou :(')
+}
 */
-
 
 /*alert('Bem-vindo ao nosso site!');
 
@@ -23,10 +26,51 @@ alert('Erro! Preencha todos os campos');
 
 let mensagemDeErro = 'Erro! Preencha todos os campos';
 alert(mensagemDeErro);*/
-let nome = prompt('Digite o seu nome');
+
+
+/*let nome = prompt('Digite o seu nome');
 
 let idade = prompt('Digite a sua idade');
 
 if (idade >= 18) {
     alert('Pode tirar a habilitação!'); 
 }
+*/
+
+
+
+
+
+
+
+/* DESAFIO 1 09/07/2025
+diaDaSemana = prompt ('Qual é o dia da semana?');
+if (diaDaSemana == 'Sabado') {
+    alert('Bom fim de semana');
+}
+else if (diaDaSemana == 'Domingo') {
+    alert ('Bom fim de semana');
+} else {
+    alert ('Boa semana');
+}
+*/
+
+
+/* DESAFIO 2 09/07/2025
+numero = prompt ('Qual é o seu número?');
+if (numero > 0 ) {
+    alert ('Número Positivo!')
+} else {
+    alert ('Número Negativo!')
+}
+*/
+
+/* DESAFIO 3 09/07/2025
+pontuação = prompt ('Digite sua pontuação');
+if (pontuação >= 100) {
+    alert ('Parabéns, você venceu! ')
+} else {
+    alert ('Tente novamente para ganhar.')
+}
+
+*/
