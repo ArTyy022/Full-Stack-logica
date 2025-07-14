af4a2f7257f897f983757871e79149f16bb467b0
ArTyy022 <toledoarthur11@gmail.com>
2025-07-14 19:38:37 -0300

    Add files via upload

---
+++ app3.js
@@ -0,0 +1,35 @@
+
+/*let contador = 1;
+while (contador <=20 ) {
+    alert(contador);
+    contador ++;
+}
+*/
+
+
+/*let contador2 = 10;
+while(contador2 >= 0) {
+    console.log(contador);
+    contador2 --;
+}
+*/
+
+/*let numeroMaximo = prompt ('Digite um numero para a contagem regresiva');
+while (numeroMaximo >= 0) {
+    console.log(numeroMaximo);
+    alert( numeroMaximo );
+
+    numeroMaximo--;
+    
+}
+*/
+
+
+let numeroMaximo = prompt ('Digite um numero para a contagem progressiva');
+let contador = 0;
+
+while (contador <= numeroMaximo) {
+    console.log(contador);
+    alert(contador);
+    contador++;
+}
---
+++ app4.js
@@ -0,0 +1,73 @@
+/*let nome = ('Arthur');
+console.log(`Olá, ${nome}`);
+alert (`Olá, ${nome}! Seja bem vindo`);
+
+let resposta = prompt ('Qual a linguagem de programação que você mais gosta?');
+ console.log(`${resposta}`);*/
+
+ /* let valor1 = 2;
+ let valor2 = 1
+let resultado = valor1 + valor2;
+
+alert(`A soma de ${valor1} e ${valor2} é igual ao ${resultado}`);
+console.log(`A soma de ${valor1} e ${valor2} é igual ao ${resultado}`) */
+
+/*let valor1 = 2;
+ let valor2 = 1
+let resultado = valor1 - valor2;
+
+alert(`A subtração de ${valor1} e ${valor2} é igual ao ${resultado}`);
+console.log(`A subtração de ${valor1} e ${valor2} é igual ao ${resultado}`); */
+
+/*let idade = prompt("Insira sua idade");
+if(idade >=18){
+    alert("Você é maior de idade")
+} else{
+    alert("Você é menor de idade");
+}*/
+
+
+
+/*let numero = prompt("Digite um número");
+if (numero >=0){
+    alert("Seu número é positivo");
+} else{
+    alert("Seu número é negativo");
+}
+*/
+
+/*let numero = 1;
+while(numero <= 10){
+    alert (numero);
+    numero ++;
+} */
+
+
+
+
+    /* let nota = 9;
+    if(nota >= 7){
+        alert ("Aprovado");
+    } else{
+        alert("Reprovado");
+    } */
+
+
+
+
+
+
+
+     /*let numeroAleatorio = Math.random () * 10 + 1;
+    console.log(numeroAleatorio);
+    alert(`Numero aleatorio é ${numeroAleatorio}`); */
+
+
+/*let numeroAleatorio = parseInt (Math.random () * 10) + 1;
+    console.log(numeroAleatorio);
+    alert(`Numero aleatorio é ${numeroAleatorio}`);  */
+
+
+    /* let numeroAleatorio = parseInt (Math.random () * 1000) + 1;
+    console.log(numeroAleatorio);
+    alert(`Numero aleatorio é ${numeroAleatorio}`); */
